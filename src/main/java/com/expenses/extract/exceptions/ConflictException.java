3ab332cadcdcae1b2e6ef15c76256b4233105249
felipemendes1994 <felipemendes1994@gmail.com>
2020-02-28 11:37:28 -0300

    Criado Exceptions

---
+++ src/main/java/com/expenses/extract/exceptions/ConflictException.java
@@ -0,0 +1,13 @@
+package com.expenses.extract.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class ConflictException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+	public ConflictException(String msg) {
+		super(msg);
+	}
+}
